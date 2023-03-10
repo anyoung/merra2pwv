295992f1b066a2ecbdefeb67e7a5a1aa1154deb7
anyoung <ayoung.astro.ru@gmail.com>
2023-03-10 12:36:23 +0100

    Support additional MERRA-2 ASCII file format
    The format of inst3_3d_asm_Np ASCII files has apparently changed.
     * In the new format the minute, pressure level, and latitude point for
       line in the data section is printed explicitly, whereas before these
       were indicated by reference as an index in the sample point vectors.
     * In the new format the sample point vectors are reported first,
       followed by the data section, whereas before the data section was
       first.
    
    This implementation accommodates both file formats and detects the
    format automatically.

--- src/parse_merra2.py
+++ src/parse_merra2.py
@@ -4,7 +4,86 @@ import re as regex
 from scipy.interpolate import interp2d, RegularGridInterpolator
 
 def get_data(filename,sites=None):
-	#"dat/MERRA2_400.inst3_3d_asm_Np.20180406.nc4.txt"
+	"""Wrapper for get_data1 and get_data2.
+	
+	This wrapper tries to identify the which format the file has and
+	calls the corresponding get_data[12] method."""
+
+	wrapped = None
+	with open(filename) as fh:
+		# header line identical for both formats
+		l1 = fh.readline()
+		# if the second line is longitudes, use data2
+		l2 = fh.readline()
+		if l2.find('lon') == 0:
+			wrapped = get_data2
+		else:
+			wrapped = get_data1
+	return wrapped(filename,sites=sites)
+
+def get_data2(filename,sites=None):
+	"""Extract data from MERRA-2 ASCII format ca. 2023"""
+	fh = open(filename)
+	lines = fh.readlines()
+
+	def get_grid_vars(line):
+		var_str = [x.strip() for x in line.split(',')[1:]]
+		var_map = dict(zip(var_str,range(len(var_str))))
+		var = np.array([float(l) for l in var_str])
+		return var,var_map
+
+	lon,lon_map = get_grid_vars(lines[1])
+	lat,lat_map = get_grid_vars(lines[2])
+	pres,pres_map = get_grid_vars(lines[3])
+	minu,minu_map = get_grid_vars(lines[4])
+	hour = minu/60.
+	lon[abs(lon) < 0.00001] = 0
+	lat[abs(lat) < 0.00001] = 0
+
+	Nhour = len(hour)
+	Nlon = len(lon)
+	Npres = len(pres)
+	Nlat = len(lat)
+
+	data = {"hour":hour,"lon":lon,"lat":lat,"pres":pres,"raw": {}}
+
+	H = np.zeros((Nhour,Npres,Nlat,Nlon))
+	O3 = np.zeros((Nhour,Npres,Nlat,Nlon))
+	QI = np.zeros((Nhour,Npres,Nlat,Nlon))
+	QL = np.zeros((Nhour,Npres,Nlat,Nlon))
+	RH = np.zeros((Nhour,Npres,Nlat,Nlon))
+	T = np.zeros((Nhour,Npres,Nlat,Nlon))
+	for line in lines[6:]:
+		for var,name in zip((H,O3,QI,QL,RH,T),("H","O3","QI","QL","RH","T")):
+			if line.find(name) == 0:
+				if line.find(name + '.lon') == 0: continue
+				val_minu = regex.findall("time=[\-]?[0-9]+(?:\.[0-9]+)?",line)[0].split('=')[1]
+				ii_time = minu_map[val_minu]
+				val_pres = regex.findall("lev=[\-]?[0-9]+(?:\.[0-9]+)?",line)[0].split('=')[1]
+				jj_pres = pres_map[val_pres]
+				val_lat = regex.findall("lat=[\-]?[0-9]+(?:\.[0-9]+)?",line)[0].split('=')[1]
+				kk_lat = lat_map[val_lat]
+				vals = np.array([float(v) for v in line.split(", ")[1:]])
+				vals[vals > 1e14] = np.nan
+				var[ii_time, jj_pres, kk_lat, :] = vals
+			data["raw"][name] = var
+
+	if sites is None:
+		return data
+
+	for site,site_lon_lat_alt in sites.items():
+		site_lon,site_lat,site_alt = site_lon_lat_alt
+		data[site] = {}
+		for var,name in zip((H,O3,QI,QL,RH,T),("H","O3","QI","QL","RH","T")):
+			data[site][name] = np.zeros((Nhour,Npres))
+			for ihour,_ in enumerate(hour):
+				for ipres,_ in enumerate(pres):
+					func = RegularGridInterpolator((lon,lat),var[ihour,ipres,:,:])
+					data[site][name][ihour,ipres] = func(np.array([[site_lon,site_lat]]))
+	return data
+
+def get_data1(filename,sites=None):
+	"""Extract data from MERRA-2 ASCII format ca. 2019"""
 	fh = open(filename)
 	lines = fh.readlines()
 
